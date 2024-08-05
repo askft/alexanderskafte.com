663d3ef3bc08cd1caf1c5722c46cf8d3ac6b00a8
Alexander Skafte <alexanderskafte@protonmail.com>
2024-08-05 22:29:02 +0200

    ...

--- docs/_next/static/9RUUGM8l8Z8nhCNdxE8wZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8dfd0fca4d5611a8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/projects":["static/chunks/pages/projects-9de776531c7d1adb.js"],sortedPages:["/","/_app","/_error","/projects"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ docs/_next/static/Kbnmd6qmHa0GjM0nwlgK9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6d7dcdc1872bfbf5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/projects":["static/chunks/pages/projects-9de776531c7d1adb.js"],sortedPages:["/","/_app","/_error","/projects"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
